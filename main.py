24a135b509c7710cd1ef4445215910a7929c4d38
yu-jeffy <105130385+yu-jeffy@users.noreply.github.com>
2022-07-05 00:48:59 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,66 @@
+import os
+import openai
+
+
+openai.api_key = os.environ['openai_key']
+
 import interactions
 
 wozniak = interactions.Client(
-    token=
-    "OTg2NTIxMzE1MjIzNjY2Njg4.G_w8qH.f6BGlrqBOs_CveJHcG_AysV1eTtsKNVzQV-nr0")
+    token= os.environ['discord_key'])
 
-import openai
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-openai.api_key = "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"
-
+async def on_ready():
+  print("Ready!")
 
 @wozniak.command(name="my_first_command",
                  description="This is the first command I made!")
 async def my_first_command(ctx: interactions.CommandContext):
     await ctx.send("Hi there!")
 
+@wozniak.command(name="ping",
+                 description="ping!")
+async def ping(ctx: interactions.CommandContext):
+    await ctx.send("pong!")
+
 
 @wozniak.command(
-    name="code4me",
+    name="ask_woz",
     description="what do you want to code?",
     options=[
         interactions.Option(
-            name="text",
+            name="input",
             description="what do you want to code?",
             type=interactions.OptionType.STRING,
             required=True,
         ),
     ],
 )
-async def getCode(ctx: interactions.CommandContext, text: str):
+async def ask_woz(ctx: interactions.CommandContext, input: str):
+  output = ""
+  
+  print("User input: " + input)
 
-    response = openai.Completion.create(engine="text-davinci-002",
-                                        prompt="Write a story based on: " +
-                                        text,
-                                        temperature=0,
-                                        max_tokens=256,
-                                        top_p=1.0,
-                                        frequency_penalty=0.0,
-                                        presence_penalty=0.0)
+  await ctx.send("Working...")
+  
 
-    outputResponse = str(response['choices'][0]['text'])
-    await ctx.send("works")
-    await ctx.send(outputResponse)
+  response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt= input,
+    temperature=0,
+    max_tokens=256,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0
+  )
+
+  output = str(response['choices'][0]['text'])
+
+  print("OpenAI Response: " + output)
+  
+  await ctx.edit(output)
 
 
 wozniak.start()
+
